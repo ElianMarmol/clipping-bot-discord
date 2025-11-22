3cc0b70a98fab5e963993ee5f66fbc134850adff
ElianMarmol <elianmarmol00@gmail.com>
2025-11-21 22:30:43 -0300

    fix metrics

--- start_all.py
+++ start_all.py
@@ -2,7 +2,10 @@ import asyncio
 from bots.main import main_bot
 from bots.main2 import admin_bot
 from bots.main3 import bot as equipos_bot
-from metrics_server import start_metrics_server  # ← IMPORTANTE
+
+# ✔ Import corregido
+from metrics_server.metrics_server import start_metrics_server
+
 import os
 from dotenv import load_dotenv
 
@@ -11,10 +14,7 @@ load_dotenv()
 async def run_all_bots():
     print("🚀 Iniciando los 3 bots + metrics_server…")
 
-    # 🔥 arranca el servidor de métricas
     task0 = asyncio.create_task(start_metrics_server())
-
-    # 🔥 arranca los bots
     task1 = asyncio.create_task(main_bot.start(os.getenv("DISCORD_MAIN_BOT_TOKEN")))
     task2 = asyncio.create_task(admin_bot.start(os.getenv("DISCORD_ADMIN_BOT_TOKEN")))
     task3 = asyncio.create_task(equipos_bot.start(os.getenv("DISCORD_EQUIPOS_BOT_TOKEN")))
